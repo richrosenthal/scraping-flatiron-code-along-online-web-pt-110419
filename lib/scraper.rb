f1e660c1431d3a7df006a84673a40e691359e1e9
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-01-19 22:55:35 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -17,13 +17,7 @@ class Scraper
   end
   
   def get_page
-    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-    
-    doc.css(".post").each do |post|
-      course = Course.new
-      course.title = post.css("h2").text
-      course.schedule = post.css(".date").text
-      course.description = post.css("p").text
+    Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
   end 
 
   def get_courses 
@@ -31,14 +25,17 @@ class Scraper
   end 
   
   def make_courses 
-    
+    self.get_courses.each do |post|
+    course = Course.new
+    course.title = post.css("h2").text
+    course.schedule = post.css(".date").text
+    course.description = post.css("p").text
+  end
   end 
   
-  def print_courses
-    
-  end 
+ 
   
 end
 
-
+Scrape.new.print_courses 
 
